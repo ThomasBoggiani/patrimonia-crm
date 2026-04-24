f15415edacec2a572143bc986b6c0f867c4bdc29
ThomasBoggiani <thomas.boggiani@immeubles-patrimoine.fr>
2026-04-24 13:27:02 +0200

    Create SmartImportModal.jsx

---
+++ components/SmartImportModal.jsx
@@ -0,0 +1,4 @@
+Index de /Users/Thomas/Documents/Immeubles et Patrimoine/CRM/files 3/mnt/user-data/outputs/MISE_A_JOUR_COMPLETE/app/api/smart-import/
+[répertoire parent]
+Nom	Taille	Date de modification
+route.js	7.2 kB	24/04/2026 10:40:34
