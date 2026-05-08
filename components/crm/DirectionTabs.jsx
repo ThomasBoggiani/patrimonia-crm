1bc79b8a6669b0035fae49c8846ef647e96ee2c8
ThomasBoggiani <thomas.boggiani@immeubles-patrimoine.fr>
2026-05-08 07:31:32 +0200

    Create DirectionTabs.jsx

---
+++ components/crm/DirectionTabs.jsx
@@ -0,0 +1,19 @@
+import {
+  Field,
+  DetailItem,
+  KpiCard,
+  KpiBox,
+  TaskRow,
+  AlertRow,
+  CommerceBadge,
+  StatutBadge,
+  DealStatutBadge,
+  MaturiteBadge,
+  TypeInteractionBadge,
+  TaskInline,
+  QuickAddTask,
+} from '@/components/crm/SharedComponents';
+import {
+  DashboardDirection,
+  RemunerationTab,
+} from '@/components/crm/DirectionTabs';
